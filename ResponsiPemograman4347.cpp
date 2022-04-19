8fa1087185dd908fcf2c67a7f49e78e36d60499b
RafaelHR1 <101814500+RafaelHR1@users.noreply.github.com>
2022-04-19 11:20:16 +0700

    Add files via upload

---
+++ ResponsiPemograman4347.cpp
@@ -0,0 +1,30 @@
+menggunakan  Sistem ;
+menggunakan  Sistem . Koleksi . Umum ;
+menggunakan  Sistem . Linq ;
+menggunakan  Sistem . teks ;
+menggunakan  Sistem . Threading . Tugas ;
+
+Namespace  Respons2283
+{
+    kelas  Karyawan
+    {
+        // default konstruktor
+         Karyawan publik ()
+        {
+
+        }
+        // membebani konstruktor
+         Karyawan publik ( string  nama , string  nik , int  gajibulanan )
+        {
+            NAMA  =  nama ;
+            NIK  =  nik ;
+            GajiBulanan  =  gajibulanan ;
+        }
+        // properti
+         NAMA string  publik       { dapatkan ; mengatur ; }
+         string  publik NIK        { dapatkan ; mengatur ; }
+        public  int  GajiBulanan   { dapatkan ; mengatur ; }
+               
+             
+    }
+}
